78f7d45928f1e03f2e5a6721dcf1a3ae8c0bfe0a
Atanas Angelov <atanas3angelov@gmail.com>
2023-07-21 22:00:28 +0300

    progress with react MuiltiChoiceConnect
    minor refactoring of JS property calling (.)->['txt'] to delimiter better pure JS vs backend(Python) code origin
    minor bug fixes
    work not committed yesterday -> pure JS eventhandling to deal with react focus limitations
    
    issues that took way too much time:
    -react state setting has a delay of 1 render
    -disabling a focused radio stuck the event listener (the solution was with a blur)

--- lmw/dictionary/src/PracticeApp.js
+++ lmw/dictionary/src/PracticeApp.js
@@ -30,6 +30,190 @@ const AudioButton = memo(({pronunciation}) => {
         return (<></>)
 });
 
+const MuiltiChoiceConnect = ({ words, trainedWords, onQuestionChange }) => {
+    console.log('render MuiltiChoiceConnect');
+
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [correctlyAnswered, setCorrectlyAnswered] = useState([]);
+    
+    const shuffleAnswers = useMemo(
+        () => {
+            let pattern = Array.from(Array(words.length).keys())
+
+            for (let i = pattern.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [pattern[i], pattern[j]] = [pattern[j], pattern[i]];
+            }
+
+            return pattern;
+        },
+        [words]);
+    
+    const allAnswers = useMemo(
+        () => {
+            let answers = [];  // each el is object: {questionWord['id'] : questionWord.translations[random]} (id: used in comparing question & answer radios)
+
+            for (let i = 0; i < shuffleAnswers.length; i++) {
+                    // use the word's id for the radio value comparison in both groups
+                    const key = words[shuffleAnswers[i]]['id'];
+                    // choose rand translation
+                    const val = words[shuffleAnswers[i]].translations[
+                        Math.floor( Math.random() * words[shuffleAnswers[i]].translations.length )
+                    ];
+                    const obj = {};
+                    obj[key] = val;
+                    answers.push(obj);
+                }
+                return answers;
+            },
+        [words, shuffleAnswers]
+    );
+
+    function handleKeyboard(e) {
+        console.log('handler');
+        for (let i = 1; i <= words.length; i++) {
+            if (e.key == i) {
+                document.getElementById(words[i-1]['id']).focus();
+                setQuestion(words[i-1]['id']);
+                checkAnswer();
+            }
+        }
+        if ((e.key == 'Enter') && (correctlyAnswered.length == words.length))
+            nextQuestion()
+    }
+
+    useEffect(() => {
+        // check anser on each re-render
+        
+
+        document.addEventListener("keydown", handleKeyboard);
+        return () => document.removeEventListener("keydown", handleKeyboard);
+    }); // runs after every re-render
+
+    // function onKey(e) {
+    //     // change radio select on 1, 2, 3
+
+    //     if (e.key == 'Enter')
+    //         checkAnswer();
+    // }
+
+    function checkAnswerInner(q, a) {
+        if (q && a) {
+
+            let word = words.find(word => word.id == q);
+
+            if (q == a) {
+                word['correct'] += 1;
+                trainedWords.push(word);
+                correctlyAnswered.push(word['id']);
+                // prevent disabling of radio to stuck listener
+                document.activeElement.blur();
+                setQuestion('');
+                setAnswer('');
+            }
+            else {
+                word['mistakes'] += 1;
+                setQuestion('');
+                setAnswer('');
+            }
+        }
+    }
+
+    function checkAnswer(e) {
+
+        console.log(question);
+        console.log(answer);
+        console.log(e);
+
+        if (e && e.target.name == 'question') {
+            if (answer) {
+                checkAnswerInner(e.target.value, answer);
+            }
+            else {
+                setQuestion(Number(e.target.value));
+            }
+        }
+
+        if (e && e.target.name == 'answer') {
+            if (question) {
+                checkAnswerInner(question, e.target.value);
+            }
+            else {
+                setAnswer(Number(e.target.value));
+            }
+        }
+
+        // if using the keyboard (it won't activate on current state change)
+        checkAnswerInner(question, answer);
+    }
+
+    // function selectQuestion(e) {
+    //     console.log(e.target);
+    //     setQuestion(Number(e.target.value));
+    //     // checkAnswer();
+    // }
+
+    // function selectAnswer(e) {
+    //     setAnswer(Number(e.target.value));
+    //     // checkAnswer();
+    // }
+
+    function nextQuestion () {
+
+        setCorrectlyAnswered([]);
+        onQuestionChange();
+    }
+
+    return (<>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        {
+                            words.map(leftWord => 
+                                <p key={ leftWord['id'] }>
+                                    <input 
+                                        id={ leftWord['id'] }
+                                        type='radio' 
+                                        name='question' 
+                                        value={ leftWord['id'] }
+                                        checked={ question == leftWord['id'] }
+                                        disabled={ correctlyAnswered.includes(leftWord['id']) } 
+                                        onChange={ checkAnswer } />
+                                    <label htmlFor={ leftWord['id'] }>
+                                        { leftWord['word_text'] }
+                                    </label>
+                                </p>
+                        )}
+                    </td>
+                    <td>
+                        {
+                            allAnswers.map(rightWord =>
+                                <p key={ rightWord[Object.keys(rightWord)[0]]['id'] }>
+                                    <input 
+                                        id={ rightWord[Object.keys(rightWord)[0]]['id'] }
+                                        type='radio' 
+                                        name='answer' 
+                                        value={ Object.keys(rightWord).map(Number)[0] } 
+                                        checked={ answer == Object.keys(rightWord)[0] }
+                                        disabled={ correctlyAnswered.includes(Object.keys(rightWord).map(Number)[0]) }
+                                        onChange={ checkAnswer } 
+                                        onKeyDown={ console.log('D') } />
+                                    <label htmlFor={ rightWord[Object.keys(rightWord)[0]]['id'] }>
+                                        { rightWord[Object.keys(rightWord)[0]]['word_text'] }
+                                    </label>
+                                </p>
+                        )}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <input id='nxt' type="button" value="Next" onClick={ nextQuestion } disabled={ correctlyAnswered.length != words.length } />
+    </>);
+}
+
 const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listening, onQuestionChange }) => {
     console.log('render MuiltiChoice');
 
@@ -57,7 +241,7 @@ const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listeni
     const allAnswers = useMemo(
         () => {
             let answers = [];
-            let uniqueIds = [correctTranslation.id];
+            let uniqueIds = [correctTranslation['id']];
             // pick 2 other random translations from otherWords while pushing correct in a random place
             for (let i = 0; i < 3; i++) {
                 if (i == randomPlaceOfCorrect) {
@@ -69,7 +253,7 @@ const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listeni
                     while(uniqueIds.includes(randOther.id))
                         randOther = otherWords[Math.floor( Math.random() * otherWords.length ) ];
                     answers.push(randOther);
-                    uniqueIds.push(randOther.id);
+                    uniqueIds.push(randOther['id']);
                 }
             }
             return answers;
@@ -81,21 +265,21 @@ const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listeni
     function handleKeyboard(e) {
         console.log(allAnswers);
         if (e.key == '1' ) {
-            document.getElementById(allAnswers[0].id).focus();
+            document.getElementById(allAnswers[0]['id']).focus();
             // document.getElementById(allAnswers[0].id).checked=true;
-            setAnswer(allAnswers[0].word_text);
+            setAnswer(allAnswers[0]['word_text']);
         }
         if (e.key == '2') {
-            document.getElementById(allAnswers[1].id).focus();
+            document.getElementById(allAnswers[1]['id']).focus();
             // document.getElementById(allAnswers[1].id).checked=true;
-            setAnswer(allAnswers[1].word_text);
+            setAnswer(allAnswers[1]['word_text']);
         }
         if (e.key == '3') {
-            document.getElementById(allAnswers[2].id).focus();
+            document.getElementById(allAnswers[2]['id']).focus();
             // document.getElementById(allAnswers[2].id).checked=true;
-            setAnswer(allAnswers[2].word_text);
+            setAnswer(allAnswers[2]['word_text']);
         }
-        if (e.key == 'Enter')
+        if ((e.key == 'Enter') && (answer))
             if (!checkingAnswer)
                 checkAnswer();
             else
@@ -127,7 +311,7 @@ const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listeni
         if (translationFrom)
             word.translations.forEach(w => correctTranslations.push(w['word_text']));
         else
-            correctTranslation.push(word['word_text']);
+            correctTranslations.push(word['word_text']);
 
         if (correctTranslations.includes(answer)) {
             word.correct += 1;
@@ -150,24 +334,24 @@ const MuiltiChoice = ({ word, translationFrom, otherWords, trainedWords, listeni
         setMessageAdditional('');
         setCheckingAnswer(false);
 
-        onQuestionChange(word);
+        onQuestionChange();
     }
 
     return (<>
-        { wordFrom.pronunciation ? <AudioButton pronunciation={ wordFrom.pronunciation } /> : <></> }
+        { wordFrom['pronunciation'] ? <AudioButton pronunciation={ wordFrom['pronunciation'] } /> : <></> }
         <label>{ wordFrom['word_text'] }</label>
 
         { allAnswers && allAnswers.map(possibleAnswer => 
-            <p key={ possibleAnswer.id }>
+            <p key={ possibleAnswer['id'] }>
                 <input 
-                    id ={ possibleAnswer.id } 
+                    id ={ possibleAnswer['id'] } 
                     value={ possibleAnswer['word_text'] }
                     checked={ answer == possibleAnswer['word_text'] }
                     { ...answerInputProps }
                     // onKeyDown={ (e) => onKey(e) }
                     disabled={ checkingAnswer } />
-                <label htmlFor={ possibleAnswer.id }>
-                    {possibleAnswer.word_text}
+                <label htmlFor={ possibleAnswer['id'] }>
+                    { possibleAnswer['word_text'] }
                 </label>
             </p>
         )}
@@ -213,7 +397,14 @@ const DirectText = ({ word, translationFrom, trainedWords, listening, onQuestion
                 Math.floor( Math.random() * word.translations.length ) ],
             [word]);
 
-
+    function handleKeyboard(e) {
+        if ((e.key == 'Enter') && (answer))
+            if (!checkingAnswer)
+                checkAnswer();
+            else
+                nextQuestion()
+    }
+        
     // useEffect(() => {
 
     // }, []); // runs only on mount
@@ -222,6 +413,9 @@ const DirectText = ({ word, translationFrom, trainedWords, listening, onQuestion
         // ref.current && ref.current.focus();
 
         document.getElementById('answer').focus();
+
+        document.addEventListener("keydown", handleKeyboard);
+        return () => document.removeEventListener("keydown", handleKeyboard);
         // function handleAction(e) {
         //     if (e.key == 'Enter')
         //         if (checkingAnswer)
@@ -240,10 +434,10 @@ const DirectText = ({ word, translationFrom, trainedWords, listening, onQuestion
     //     //     ref2.current && ref2.current.focus();
     // }); // runs after every re-render
 
-    function onEnter(e) {
-        if (e.key == 'Enter' && e.target.value)
-            checkAnswer();
-    }
+    // function onEnter(e) {
+    //     if (e.key == 'Enter' && e.target.value)
+    //         checkAnswer();
+    // }
 
     function checkAnswer () {
 
@@ -285,7 +479,7 @@ const DirectText = ({ word, translationFrom, trainedWords, listening, onQuestion
         <label>{ wordFrom['word_text'] }</label>
         <input id="answer" type="text" autoComplete="off" value={ answer } 
             onChange={ (e) => setAnswer(e.target.value) }
-            onKeyDown={ (e) => onEnter(e) }
+            // onKeyDown={ (e) => onEnter(e) }
             disabled={ checkingAnswer } />
 
         { checkingAnswer ? 
@@ -424,6 +618,23 @@ const PracticePane = ({ session, words, otherWordsFrom, otherWordsTo }) => {
                     onQuestionChange={ nextQuestion } />;
             break;
         case 'multiple_choice_connect':
+            // get 4 words to practice or the amount remaining
+            const mccWords = [];
+            // remember the word currently in the react state 'word'!
+            const mccNumOfWords = (words.length + 1 >= 4) ? 4 : words.length + 1;
+
+            mccWords.push(word);
+            // for (let i = 1; i < mccNumOfWords; i++) {
+            //     mccWords.push(words.shift());
+            // }
+            mccWords.push(...words.splice(0, mccNumOfWords-1));
+
+            questionAnswer = 
+                <MuiltiChoiceConnect 
+                    words={ mccWords } 
+                    trainedWords={ trainedWords } 
+                    onQuestionChange={ nextQuestion }/>
+            break;
         case 'listening':
         case 'listening_multiple_choice':
         default:
@@ -435,6 +646,7 @@ const PracticePane = ({ session, words, otherWordsFrom, otherWordsTo }) => {
             <input type="submit" value="End" onClick={() => end()} />
         </div>
         <div id="practice_pane">
+            { word ? translationFrom ? <h4>Translate from {session['lang']}:</h4>: <h4>Translate to {session['lang']}</h4> : <></> }
             { word ? questionAnswer : <></>}
         </div>
     </div>)
@@ -484,7 +696,7 @@ const PracticeApp = () => {
 
     return (<>
         <div>
-            { err ? <p style={{color: 'red'}}><strong> {{ err }} </strong></p> : <></> }
+            { err ? <p style={{color: 'red'}}><strong> { err } </strong></p> : <></> }
         </div>
         { words.length > 0 ? 
             <PracticePane session={session} words={words} 
